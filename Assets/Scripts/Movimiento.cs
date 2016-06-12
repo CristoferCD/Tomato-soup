9cd9802b8e80ccb1580f8d6ad94a2e5b6919fc52
Cristofer <cristofercnsdmngz@live.com>
2016-06-12 17:54:45 +0200

    Concepto de control
    Salto en la dirección de la ramita y va a cámara lenta para decidir la
    dirección del siguiente salto.

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class Movimiento : MonoBehaviour {
 
+    private Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(Input.GetAxis("Horizontal") * 3 * Time.deltaTime, 0.0f), ForceMode2D.Force);
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, .2f), ForceMode2D.Impulse);   //Esto es lento
+        rb.AddTorque(-Input.GetAxis("Horizontal") * Time.deltaTime);
+        //rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * 3 * Time.deltaTime, 0.0f));
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(.3f * transform.up, ForceMode2D.Impulse);
         }
+        if (rb.velocity.y < -0.05) {
+            Time.timeScale = 0.5f;
+            rb.angularVelocity = 720 * (-Input.GetAxis("Horizontal"));
+        } else
+            Time.timeScale = 1;
 	}
 }
